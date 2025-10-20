826d1045512b001885e8d6d0fbb8f2efa5206c8d
gang.li02 <gang.li02@mthreads.com>
2025-10-20 19:58:50 +0800

    add dunst
    Signed-off-by: gang.li02 <gang.li02@mthreads.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 # Execute initialization only on first run
 xrdb merge ~/.config/chadwm/.Xresources
 xbacklight -set 10 & # Commented out, usually not needed for desktop PCs
-# set 120hz 
+# set 120hz
 xrandr --output eDP-1 --mode 0x4b
 # set touchpad natutral scrolling
 xinput set-prop 10 "libinput Natural Scrolling Enabled" 1
@@ -11,12 +11,16 @@ xinput set-prop 10 "libinput Tapping Enabled" 1
 xinput set-prop 10 "libinput Click Method Enabled" 1 0
 xinput set-prop 10 "libinput Disable While Typing Enabled" 1
 
-feh --bg-fill --randomize ~/Pictures/wall/* &
+feh --bg-fill --randomize /usr/share/backgrounds/* &
 picom &
 redshift -l 30.6:114.3 -t 6500:4000 & # Auto night mode for Wuhan location
 pot &
 flameshot &
 fcitx5 &
+dunst &
+nm-applet &
+blueman-applet &
+pasystray &
 
 gestures start &
 dash ~/.config/chadwm/scripts/bar.sh &
